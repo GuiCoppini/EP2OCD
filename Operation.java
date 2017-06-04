18ba4049fde07d78a7496fc62299d6bb0ceaee61
GuiCoppini <guilherme.pavani@usp.br>
2017-06-04 01:12:16 -0300

    Implement Operation.java

--- Operation.java
+++ Operation.java
@@ -1,14 +1,10 @@
 public class Operation {
-  static execute(String line) {
-    if(line.substring(0, 2).equals("mov")) { // é um mov
-      if (line.charAt(4) == '[') { // primeira parte é da memoria
-        //memory.get(dentroDos[])
-      }
+  String opcode;
+  public Operation(String opcode) {
+    this.opcode = opcode;
+  }
 
-      if (line.charAt(line.length-1) == ']') { // segunda parte é da memoria
-        //memory.get(dentroDos[])
-        //coloca la dentro a parte que vem antes da virgula
-       }
-    }
+  public getOp(){
+    return opcode;
   }
 }
--- OperationCode.java
+++ OperationCode.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class OperationCode {
-  public static HashMap<String,Operacao > opCodes = new HashMap<String,Operacao>();
+  public static HashMap<String,Operation> opCodes = new HashMap<String,Operation>();
   static void criaMapaPadronizado() { //Cria mapa contendo as opCodes
-     String[] line = {"MOV [],","MOV ,[]","MOV","MOV [],[]","ADD","SUB","MUL","DIV","INC","DEC","CMP","JMP","JE","JNE",
-                      "JG","JL","JGE","JLE","&","|"};
+    String[] line = {"MOV [],","MOV ,[]","MOV","MOV [],[]","ADD","SUB","MUL","DIV","INC","DEC","CMP","JMP","JE","JNE",
+                     "JG","JL","JGE","JLE","&","|"};
     String[] opCode = {"00001","00010","00011","00100","00101","00110","00111","01000",
-                        "01001","01010","01011","01100","01101","01110","01111","10000","10001",
-                        "10010","10011","10100"};
+                       "01001","01010","01011","01100","01101","01110","01111","10000","10001",
+                       "10010","10011","10100"};
     int i = 0;
     while(i < line.length){
-      Operacao op = new Operacao(opCode[i]);
+      Operation op = new Operation(opCode[i]);
       opCodes.put(line[i], op);
       //System.out.println(line[i] +": "+opCodes.get(line[i]).get);
       i++;
@@ -27,7 +27,7 @@ public static String recupera(String a){ //Recupera a opCode da String de instru
             return opCodes.get("MOV ,[]").getOp();
         }
     }
-    Operacao op = opCodes.get(comando(a));
+    Operation op = opCodes.get(comando(a));
     if(op != null)
     return op.getOp();
     return null;
