4a083c52afd866a8bfb0538f21328af4232f9084
GuiCoppini <guilherme.pavani@usp.br>
2017-06-04 01:05:27 -0300

    Create ifs to mov

--- OperationCode.java
+++ OperationCode.java
@@ -1,19 +1,48 @@
+import java.util.*;
 public class OperationCode {
-  static HashMap<String,Operacao > opCodes = new HashMap<String,Operacao>();
-  static void execute(String [] line , String [] opCode) {
+  public static HashMap<String,Operacao > opCodes = new HashMap<String,Operacao>();
+  static void criaMapaPadronizado() { //Cria mapa contendo as opCodes
+     String[] line = {"MOV [],","MOV ,[]","MOV","MOV [],[]","ADD","SUB","MUL","DIV","INC","DEC","CMP","JMP","JE","JNE",
+                      "JG","JL","JGE","JLE","&","|"};
+    String[] opCode = {"00001","00010","00011","00100","00101","00110","00111","01000",
+                        "01001","01010","01011","01100","01101","01110","01111","10000","10001",
+                        "10010","10011","10100"};
     int i = 0;
     while(i < line.length){
       Operacao op = new Operacao(opCode[i]);
       opCodes.put(line[i], op);
-      System.out.println(line[i] +": "+opCodes.get(line[i]).opCode);
+      //System.out.println(line[i] +": "+opCodes.get(line[i]).get);
       i++;
     }
-  }
+}
+
+public static String recupera(String a){ //Recupera a opCode da String de instrução
+    if(comando(a).equalsIgnoreCase("mov")){
+        if(a.charAt(4) == '['){
+            if(a.charAt(a.length()-1) == ']'){
+                return opCodes.get("MOV [],[]").getOp();
+            }
+            return opCodes.get("MOV [],").getOp();
+        } else if(a.charAt(a.length() - 1) == ']'){
+            return opCodes.get("MOV ,[]").getOp();
+        }
+    }
+    Operacao op = opCodes.get(comando(a));
+    if(op != null)
+    return op.getOp();
+    return null;
+}
+private static String comando(String palavra){
+    int i = 0;
+    String tempo = palavra;
+    String[] temp = tempo.split(" ");
+    String comando = temp[0];
+    return comando;
+}
   public static void main(String [] args){
-    String[] teste = {"MOV","ADD","SUB","MUL","DIV","INC","DEC","CMP","JMP","JE","JNE",
-                      "JG","JL","JGE","JLE","&","|"};
-    String[] opCodes = {"00001","00010","00011","00100","00101","00110","00111","01000",
-                        "01001","01010","01011","01100","01101","01110","01111","10000","10001"};
-    execute(teste , opCodes); 
+    criaMapaPadronizado(); 
+    Scanner scan = new Scanner(System.in);
+    String a = scan.nextLine();
+    System.out.println(recupera(a));
   }
 }
