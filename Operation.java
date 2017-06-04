741ef24960e51cdcdb289767a1fa106ead0696e6
GuiCoppini <guilherme.pavani@usp.br>
2017-06-03 21:16:44 -0300

    Cria as porra tudo

---
+++ Operation.java
@@ -0,0 +1,14 @@
+public class Operation {
+  static execute(String line) {
+    if(line.substring(0, 2).equals("mov")) { // é um mov
+      if (line.charAt(4) == '[') { // primeira parte é da memoria
+        //memory.get(dentroDos[])
+      }
+
+      if (line.charAt(line.length-1) == ']') { // segunda parte é da memoria
+        //memory.get(dentroDos[])
+        //coloca la dentro a parte que vem antes da virgula
+       }
+    }
+  }
+}
