a4fdc6c023aeea75fab47c3eba55cfc4a8d46893
stat2008 <stat2007@hotmail.com>
2017-06-05 19:46:07 -0300

    falta soh implementar a execucao das funcoes

--- Operation.java
+++ Operation.java
@@ -1,9 +1,74 @@
 public class Operation {
-    private String opCode;
-    public Operation(String opCode){
-        this.opCode = opCode;
-    }
-    public String getOp(){
-        return this.opCode;
-    }
+    //private String opCode;
+	static String[] opCode = {"00001","00101","00110","00111","01000",
+                       "01001","01010","01011","01100","01101","01110","01111","10000","10001",
+                       "10010","10011","10100"};
+					static    String[] line = {"MOV","ADD","SUB","MUL","DIV","INC","DEC","CMP","JMP","JE ","JNE",
+                     "JG ","JL ","JGE","JLE","&","|"}; // nao tira os espacos pfvr
+		//mapear pra cada operacao qual eh o microcodigo correspondente pfvr
+	public static void executa(int op){
+		int posicao = find(op);
+		switch (posicao) {
+			case 0: //mov(vale lembrar q temos 3 casos dentro do mov)
+			System.out.println("MOV");
+			break;
+			case 1: //add
+			System.out.println("ADD");
+			break;
+			case 2: //sub
+			System.out.println("SUB");
+			break;
+			case 3:// mul
+			System.out.println("MUL");
+			break;
+			case 4:// div
+			System.out.println("DIV");
+			break;
+			case 5://inc
+			System.out.println("INC");
+			break;
+			case 6://dec
+			System.out.println("DEC");
+			break;
+			case 7://cmp
+			System.out.println("CMP");
+			break;
+			case 8://jmp
+			System.out.println("JMP");
+			break;
+			case 9://je
+			System.out.println("JE");
+			break;
+			case 10://jne
+			System.out.println("JNE");
+			break;
+			case 11://jg
+			System.out.println("JG");
+			break;
+			case 12://jl
+			System.out.println("JL");
+			break;
+			case 13://jge
+			System.out.println("JGE");
+			break;
+			case 14://jle
+			System.out.println("JLE");
+			break;
+			case 15://&
+			System.out.println("&");
+			break;
+			case 16://|
+			System.out.println(" | ");
+			break;
+		}
+		//imprime o microcodigo pfvr;
+	}
+	private static  int find(int a){ // encontra dentro do array de operacoes, qual estamos executando e retorna a posicao
+		String comando = Principal.memoria[a].substring(0,3);
+		for(int h = 0 ; h < line.length ; h++){
+			if(comando.equals(line[h]) ) return h;
+		}
+		System.out.println("äaaaaaaaaeeeeeeeeeeeeeeeeeehooooooooooooooooooooooooooooo nao achou a string"); // obviamente nao queremos chegar aqui
+		return -1;
+	}
 }
--- Principal.java
+++ Principal.java
@@ -1,40 +1,75 @@
 import java.util.*;
+import java.io.BufferedReader ;
+import java.io.FileReader ;
 public class Principal {
   static int wordSize = 36; // Tamanho da palavra
-  static boolean[] ax = new boolean[wordSize]; 
-  static boolean[] bx = new boolean[wordSize]; 
-  static boolean[] cx = new boolean[wordSize]; 
-  static boolean[] dx = new boolean[wordSize]; 
+  static int regsize = 16; // reg nao precisa guardar uma word toda // na memoria 5 bit pra opcode e 11 pra var // enderecos 16bits // entende numeros ate 2048 unsigned, acho q cobre o escopo do problema
+  static boolean[] ax = new boolean[regsize]; 
+  static boolean[] bx = new boolean[regsize]; 
+  static boolean[] cx = new boolean[regsize]; 
+  static boolean[] dx = new boolean[regsize]; 
   static boolean cf, zf, sf, of;
-  static HashMap<Boolean[], String> memory = new HashMap<Boolean[], String>();
-  boolean[] pc = new boolean[wordSize];
-  Stack<String> stack = new Stack<>();
+ // static HashMap<Boolean[], String> memory = new HashMap<Boolean[], String>(); // pelo q eu pesquisei hashmap n serve aqui pq se perde a posicao
+  static String[] memoria = new String[65536]; //  cobre todos os valores do enderecamento  //pesado quase nada
+  static int pc = 0;
+/*
+You can get a binary string from an integer like so:
 
-//  public static void main(String[] args) {
+int i = 1234;
+String binString = Integer.toBinaryString(i);
+and you can convert the string back to an integer this way:
+
+int iNew = Integer.parseInt(binString, 2);
+
+*/
+
+  public static void main(String[] args) {
     // PC Settado pra primeira linha do codigo na memoria
-//    while(true) {
-//      String instruction = memory.get(pc); // pega uma linha
-//      Operation.execute(instruction); // le a linha
-//      pc.parseInt // pc <- pc + 1, le prox linha
-//    }
-//  }
-
-  // public static boolean[] somaArrays(boolean[] a, boolean[] b) {
-  //   for(int i = b.length-1; i>=0; i--) {
-  //     boolean carry = false;
-  //     if (b[i] == true && a[i] == true) {
-  //       carry = true;
-  //       b[i] = false;
-        
-  //     }
-  //     if (b[i] != a[i]) {
-  //       carry = false;
-  //       b[i] = true;
-  //     }
-  //     if (b[i] == false && a[i] == false) {
-  //       carry = false;
-  //       b[i] = false;
-  //     }
-  //   }
-  // }
+    //"stack" do codigo cresce de cima pra baixo, stack das variaveis cresce de baixo pra cima, logo primeira linha do codigo esta em memoria[0] e a primeira var ta em memoria[memoria.length]
+	le_codigo_do_arquivo(); //metodo magico q n implementei, le o codigo do arq e salva na memoria, adcionar operacao EXIT no fim do codigo se 
+	int count = 0;
+	//while !exit
+	while(!Principal.memoria[count].substring(0,3).equals("EXI")){
+	pc = count; // pc guarda o endereco da instrucao na memoria
+	Operation.executa(pc); // a magica acontece aqui
+	System.out.println("AX");
+	imprime(ax);
+	System.out.println("BX");
+	imprime(bx);
+	System.out.println("CX");
+	imprime(cx);
+	System.out.println("DX");
+	imprime(dx);
+	System.out.println("Cf:"+cf+" Zf:"+zf+" Sf:"+sf+" Of:"+of);
+	//imprime(pc);
+	System.out.println("PC: "+pc);
+	System.out.println("Endereco da instrucao eh: "+count);
+	System.out.println("/////////////////////////////////////////////////////////////////");
+	count++;
+	}
+System.out.println("Fim da execucao");
+  }
+  
+  public static void imprime(boolean[] a){ // autoexplicativo
+	  for(int i = 0 ; i < a.length ; i++){
+		  System.out.print(a[i] ? "1 ": "0 "); 
+	  }
+	  System.out.println();
+  }
+  public static void le_codigo_do_arquivo(){
+	  try{
+				// Lê o arquivo
+				BufferedReader reader = new BufferedReader(new FileReader("codigo.txt"));
+				String line;
+				line = reader.readLine();
+				int a = 0;
+				while(line != null){
+					memoria[a] = line;
+					a++;
+					line = reader.readLine();
+				}
+				memoria[a] = "EXIT";
+  }
+  catch(Exception e){};
+}
 }
