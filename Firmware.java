5db06efea52a325937fc332e7430f49d5f02ce42
Lucas Capalbo <lucas.capalbo@usp.br>
2017-07-05 19:22:09 -0300

    Começa a manipular bit a bit

---
+++ Firmware.java
@@ -0,0 +1,9 @@
+
+public class Firmware {
+    private boolean[][] microInstrucoes = new boolean[400][130];
+//aqui vai a matrizona que contem todas as micro instrucoes. @COPS favor fazer uma funcao pra gente comecar a editar essa matrizona
+    void criaMicroInstrucao(){
+        //vou pegar e criar linha a linha cada microInstrucao. 
+        //
+    }
+}
